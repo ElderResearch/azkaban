511bf49089308559826f40684a30fd2ea97737ae
Ryan Sharp <ryan.sharp@elderresearch.com>
2020-05-29 16:34:00 -0400

    WIP job and source detail servlet

--- azkaban-web-server/src/main/java/azkaban/webapp/servlet/CEPtorSourceDetailServlet.java
+++ azkaban-web-server/src/main/java/azkaban/webapp/servlet/CEPtorSourceDetailServlet.java
@@ -1,11 +1,16 @@
 package azkaban.webapp.servlet;
 
+import azkaban.db.schema.tables.daos.ExecutionMetricsDao;
+import azkaban.db.schema.tables.interfaces.IExecutionMetrics;
+import azkaban.db.schema.tables.pojos.ExecutionMetrics;
 import azkaban.executor.ExecutableFlow;
 import azkaban.executor.ExecutorManagerAdapter;
 import azkaban.executor.ExecutorManagerException;
 import azkaban.project.ProjectManager;
 import azkaban.server.session.Session;
 import azkaban.webapp.AzkabanWebServer;
+import lombok.Getter;
+
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
@@ -15,31 +20,46 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.joda.time.format.DateTimeFormat;
 
+import com.google.common.collect.Lists;
+
+
+
 public class CEPtorSourceDetailServlet extends LoginAbstractAzkabanServlet{
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	private ExecutorManagerAdapter executorManagerAdapter;
-	//private ProjectManager projectManager;
+	private ExecutionMetricsDao metricsDao;
 	
 	@Override
 	  public void init(final ServletConfig config) throws ServletException {
 	    super.init(config);
 	    final AzkabanWebServer server = (AzkabanWebServer) getApplication();
-	    this.executorManagerAdapter = server.getExecutorManager();
+	    //this.executorManagerAdapter = server.getExecutorManager();
+	   
 	    //this.projectManager = server.getProjectManager();
 	  }
 
 	@Override
 	protected void handleGet(HttpServletRequest req, HttpServletResponse resp, Session session)
 			throws ServletException, IOException {
+		final String EXECUTION_ID_PARAM = "execution_id";
+		final String SOURCE_DETAIL_PAGE = "azkaban/webapp/servlet/velocity/CEPtorsourcedetailpage.vm";
+
+		
 		final Page page =
-		        newPage(req, resp, session,
-		            "azkaban/webapp/servlet/velocity/CEPtorsourcedetailpage.vm");
+		        newPage(req, resp, session,SOURCE_DETAIL_PAGE);
+		
+		int executionId = getIntParam(req, EXECUTION_ID_PARAM, 0);
+		
+		if(executionId == 0) {
+			// Handle default with no execution ID
+		}
 		
 		
+		page.add("metrics",Lists.transform(metricsDao.fetchByExecutionId(executionId),
+				ExecutionMetricView::new));
 		
 		page.render();
 		
@@ -50,4 +70,14 @@ public class CEPtorSourceDetailServlet extends LoginAbstractAzkabanServlet{
 			throws ServletException, IOException {		
 	}
 
+	@Getter
+	private static class ExecutionMetricView extends ExecutionMetrics{
+		private String color;
+		
+		public ExecutionMetricView(IExecutionMetrics m) {
+			from(m);
+			color = m.getValue().intValue() > 10 ? "#FFFFFF" : "#ABCDEF";
+		}
+
+	}
 }
