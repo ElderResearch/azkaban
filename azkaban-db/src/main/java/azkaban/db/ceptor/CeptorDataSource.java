10012343af12b187cee27d8db4688a796b9befc5
Ryan Sharp <ryan.sharp@elderresearch.com>
2020-12-09 12:39:36 -0500

    refactor ceptor data source

--- azkaban-db/src/main/java/azkaban/db/ceptor/CeptorDataSource.java
+++ azkaban-db/src/main/java/azkaban/db/ceptor/CeptorDataSource.java
@@ -1,8 +1,10 @@
 package azkaban.db.ceptor;
 
 import java.sql.Connection;
+import java.sql.Driver;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Enumeration;
 import java.util.Map;
 import java.util.logging.Logger;
 
@@ -16,32 +18,22 @@ import azkaban.utils.Props;
 public class CeptorDataSource {
 	private Map<Integer, Configuration> jobConfigMap;
 	private Props props;
-	private static final String PROJECT_DB_FILE_NAME = "project_";
+	private static final String PROJECT_DB_FILE_NAME = "Project_";
 	private static final Logger logger = Logger.getLogger("CeptorDataSource");
 	
 	public CeptorDataSource(Props p) {
 		this.props = p;
 	}
 	
-	public Configuration getJobConfiguration(int projectID) {
-		return jobConfigMap.computeIfAbsent(projectID, $ -> {
-			Connection conn;
-			try {
-				conn = DriverManager.getConnection("jdbc:sqlite:"
-						+ props.getString("ceptor.databaserootpath") 
-						+ "/" + PROJECT_DB_FILE_NAME + projectID);
-				return new DefaultConfiguration()
-						.set(SQLDialect.SQLITE)
-						.set(conn);
-			} catch (SQLException e) {
-				logger.warning("Failed to connect to ceptor output DB for project " + projectID);
-				return null;
-			}
-		});
-	}
-	
-	
-	public DSLContext getJobContext(int projectID) {
-		return getJobConfiguration(projectID).dsl();
+	public DSLContext getJobContext(int projectID) throws SQLException{
+		String jdbcPath = "jdbc:sqlite:"
+				+ props.getString("ceptor.database.path")
+				+ "/" + PROJECT_DB_FILE_NAME + projectID + ".db";
+		
+		Connection conn = DriverManager.getConnection(jdbcPath);
+
+		return new DefaultConfiguration()
+				.set(SQLDialect.SQLITE)
+				.set(conn).dsl();
 	}
 }
